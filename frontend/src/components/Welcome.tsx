856836ed0c8224b7cf41a50c81927277f4b07beb
Arian Farzad <arian.farzad@stud.h-da.de>
2024-12-10 14:56:44 +0000

    Welcome and Register page design

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../style/welcom.css';
 
 const Welcome: React.FC = () => {
   const navigate = useNavigate();
@@ -13,10 +14,11 @@ const Welcome: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Welcome to SkillSwap</h2>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleRegister}>Register</button>
+    <div className='main-container'>
+      <h2 id='welcome-headline'>Welcome to SkillSwap</h2>
+      <p id='welcome-text'>This platform offers you the opportunity to connect with other participants to share your skills or work on improving them. 
+        <span className='link' onClick={handleRegister}> Register </span> if you’re here for the first time, or 
+        <span className='link' onClick={handleLogin}> log in </span> if you’re already part of our platform.</p>
     </div>
   );
 };
