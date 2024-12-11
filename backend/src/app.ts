3769c97cf8985f48f5cbdf3997aa30a5eec2d3a7
Josefu <yusuf.birdane16@gmail.com>
2024-12-11 23:55:58 +0100

    update dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,13 +18,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/profiles', profileRoutes);
 
 // MongoDB-Verbindung
-mongoose.connect(process.env.MONGO_URI!, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
+mongoose.connect(process.env.MONGO_URI!)
+    .then(() => {
+    console.log('MongoDB connected');
 }).catch((error) => {
-  console.error('MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error);
 });
 
 export default app;
