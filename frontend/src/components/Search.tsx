deaf26a73516fd8278958212c42a1d279b4f2a73
Bogdan Polskiy <bogdan.polskiy03@gmail.com>
2024-12-12 16:51:38 +0100

    fix: Search page view

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -16,7 +16,7 @@ const Search: React.FC = () => {
 
   const fetchProfiles = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/api/profiles');
+      const response = await axios.get('http://localhost:8000/api/profiles/all');
       setProfiles(response.data);
     } catch (error) {
       if (axios.isAxiosError(error)) {
