fc2346c7a5c0675fc19f630809299540266e7fbc
Bogdan Polskiy <bogdan.polskiy03@gmail.com>
2024-12-13 14:20:42 +0100

    fix: backend tests (#13)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+import cors from 'cors'; // Importieren Sie das cors-Paket
 import authRoutes from './routes/authRoutes';
 import profileRoutes from './routes/profileRoutes';
-import sessionRoutes from './routes/sessionRoutes';
-import feedbackRoutes from './routes/feedbackRoutes';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,24 +10,19 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Aktivieren Sie CORS
 app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/profiles', profileRoutes);
-app.use('/api/sessions', sessionRoutes);
-app.use('/api/feedback', feedbackRoutes);
 
 // MongoDB-Verbindung
-mongoose.connect(process.env.MONGO_URI!, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-}).then(() => {
-  console.log('MongoDB connected');
+mongoose.connect(process.env.MONGO_URI!)
+    .then(() => {
+    console.log('MongoDB connected');
 }).catch((error) => {
-  console.error('MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error);
 });
 
 export default app;
--- backend/src/tests/authRoutes.test.ts
+++ backend/src/tests/authRoutes.test.ts
@@ -8,8 +8,7 @@ dotenv.config({ path: '.env.test' });
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
   });
 });
 
