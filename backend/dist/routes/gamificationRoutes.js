dca95cca625310551282e3c9cdbfa6e90f52ecfd
Dias Baikenov <dias.baikenov@stud.h-da.de>
2024-12-15 18:11:35 +0000

    Resolve "CI/CD Pipelines"

--- backend/dist/routes/gamificationRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const gamificationController_1 = require("../controllers/gamificationController");
-const jwt_1 = require("../utils/jwt");
-const router = (0, express_1.Router)();
-router.put('/points', jwt_1.verifyToken, gamificationController_1.addPoints);
-router.get('/points', jwt_1.verifyToken, gamificationController_1.getPoints);
-exports.default = router;
